0f9ef7e4e1e69209ef9de3a8fa8b4b83a0c83ed9
Alain Landry <alinolandry@gmail.com>
2020-06-04 13:22:36 -0400

    admin ui componennt and grid custom

--- app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
@@ -10,8 +10,8 @@ class Index extends \Magento\Backend\App\Action
     public function execute()
     {
         /** @var \Magento\Framework\Controller\Result\Raw $result */
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Hello Backend');
-        return $result;
+       // $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+       // $result->setContents('Hello Backend');
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
