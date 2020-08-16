c34fc5d7ff49b0b44b9f577013443e4cfbe55930
Alain Landry <alain.landry@canac.ca>
2020-08-15 23:00:41 -0400

    add and understand setup upgrade Script

---
+++ app/code/SimplifiedMagento/Database/Setup/UpgradeSchema.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright © FSW, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Mastering\SampleModule\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if(version_compare($context->getVersion(), '1.0.1', '<')){
+            $setup->getConnection()->addColumn(
+                $setup->getTable('mastering_sample_item'),
+                'description',
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'nullable' => true,
+                    'comment' => 'Item Description'
+                ]
+            );
+        }
+        if(version_compare($context->getVersion(), '1.0.2', '<')){
+            $setup->getConnection()->addColumn(
+                $setup->getTable('sales_order_grid'),
+                'base_tax_amount',
+                [
+                    'type' => Table::TYPE_DECIMAL,
+                    'comment' => 'Base Tax Amount'
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
